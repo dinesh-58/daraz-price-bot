0623028e0a191c200ec1990569f6c4db0c894647
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2024-11-10 01:06:53 +0545

    add sqlitecloud, working scraper
    moved scraper code to bot.js for now due to module errors

--- bot.js
+++ bot.js
@@ -1,18 +1,23 @@
 import { Bot } from "grammy";
+import { Database } from '@sqlitecloud/drivers';
+import puppeteer from 'puppeteer-core';
 
 const bot = new Bot(process.env.BOT_API_TOKEN); 
-const productData = {
-    url: '',
-    idSku: '',
-    prevUndiscountedPrice: 0,
-    name: ''
-};
+const db = new Database(process.env.SQLITE_CLOUD_URL);
+
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Send any daraz product link to track when its price decreases. If the bot is offline, it will respond to your messages when it is back online"));
 // bot.on("message", (ctx) => ctx.reply("Got another message!"));
-bot.hears(/https:\/\/www\.daraz\.com\.np\/products\/[^\s]+/, ctx => {
-    clearObjectValues(productData);
-    productData.url = ctx.match[0];
+bot.hears(/https:\/\/www\.daraz\.com\.np\/products\/[^\s]+/, async (ctx) => {
+    const url = ctx.match[0];
+    console.log(`\nReceived: ${url}`);
+    
+
+    const pData = await scrapeDaraz(url);
+    console.log({pData});
+    // todo: get data from db for current product then compare
+    // if pData.finalPrice < prevPrice, notify user, then store new price
+    // if finalPrice > prevPrice, don't notify but store
 })
 
 
@@ -35,4 +40,71 @@ function clearObjectValues(obj) {
         }
     }
     return obj;
+}
+
+
+const CRON_INTERVAL_MS = 4 * 60 * 60 * 1000;
+
+function firstScrape() {}
+function scrapeLoop() {
+// setTimeout(scrapeDaraz(), CRON_INTERVAL_MS);
+}
+
+function getNumericPrice(str) {
+  return Number(str.replace(/Rs\.?\s?|,|\s/g, ""));
+}
+
+console.log(getNumericPrice("Rs. 9,000"));
+
+async function scrapeDaraz(url) {
+    const productData = {
+        id: '',
+        sku: '',
+        name: '',
+        url: '',
+        finalPrice: 0,
+        // properties starting with _ shouldn't be stored in db
+        _undiscountedPrice: 0,
+        _discountPercent: 0
+    };
+
+  const browser = await puppeteer.launch({
+    executablePath: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
+  });
+  const page = await browser.newPage();
+
+  // const url="https://www.daraz.com.np/products/m160-led-mouse-with-rgb-led-light-i128305627-s1035567228.html";
+  await page.goto(url, {timeout: 2*60*1000});
+  
+  await page.setViewport({width: 1080, height: 1024});
+
+  // TODO: make dummy site have form for changing product data. 
+//   this will then be saved to pdpTrackingData globally-scoped obj 
+
+  // INFO: each product page in daraz has a globally-scoped obj pdpTrackingData 
+  // that has data about the product
+  const loggedData = await page.evaluate(() => {
+    return typeof pdpTrackingData !== 'undefined' ? pdpTrackingData : null;
+  });
+
+  
+  productData.url = url;
+  productData.id = loggedData.pdt_sku;
+  productData.sku = loggedData.pdt_simplesku;
+  productData.name = loggedData.pdt_name;
+  productData._undiscountedPrice = getNumericPrice(loggedData.pdt_price);
+  productData.finalPrice = getNumericPrice(await page.$eval('.pdp-price_type_normal', el => el.textContent));
+
+  // maybe notify user about discount %
+  if(productData.finalPrice < productData._undiscountedPrice) {
+    console.log("discount");
+    
+      // todo: prob add condiion to check if element w/ this discount class exists or not
+      productData.discountPercent = Number(await page.$eval('.pdp-product-price__discount', el => el.textContent.replace(/-|%/g, '')));
+  }
+  
+  
+  // don't close if scraping multiple pages? maybe create separate fn to close browser & call when last product reached
+  await browser.close();
+  return productData;
 }
---
+++ commands.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products (
+    idSku TEXT PRIMARY KEY,       
+    name TEXT NOT NULL,           
+    url TEXT NOT NULL,            
+    prevPrice REAL    
+);
