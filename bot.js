b940447489de09ed613a043580319784bafa6f54
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2024-11-11 15:04:09 +0545

    create demo page w/ working api calls

--- bot.js
+++ bot.js
@@ -1,72 +1,129 @@
 import { Bot } from "grammy";
 import { Database } from '@sqlitecloud/drivers';
 import puppeteer from 'puppeteer-core';
+import express from 'express';
 
-const bot = new Bot(process.env.BOT_API_TOKEN); 
+const app = express();
+const bot = new Bot(process.env.BOT_API_TOKEN);
 const db = new Database(process.env.SQLITE_CLOUD_URL);
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+// accept json data sent to server
+app.use(express.json());
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Send any daraz product link to track when its price decreases. If the bot is offline, it will respond to your messages when it is back online"));
-// bot.on("message", (ctx) => ctx.reply("Got another message!"));
 bot.hears(/https:\/\/www\.daraz\.com\.np\/products\/[^\s]+/, async (ctx) => {
-    const url = ctx.match[0];
-    console.log(`\nReceived: ${url}`);
-    
+  const url = ctx.match[0];
+  console.log(`\nReceived: ${url}`);
 
-    const pData = await scrapeDaraz(url);
-    console.log({pData});
-    // todo: get data from db for current product then compare
-    // if pData.finalPrice < prevPrice, notify user, then store new price
-    // if finalPrice > prevPrice, don't notify but store
+
+  const pData = await scrapeDaraz(url);
+  console.log({ pData });
+  // todo: get data from db for current product then compare
+  // if pData.finalPrice < prevPrice, notify user, then store new price
+  // if finalPrice > prevPrice, don't notify but store
+  ctx.reply(`Noted. You will be notified about future price drops for ${pData.name}.`);
 })
 
+const DEMO_CRON_INTERVAL_MS = 30 * 1000;
+// no. of times to run cron job
+const DEMO_CRON_RUN_LIMIT = 5;
+bot.command("demo", ctx => {
+  // reply with link to served demo html file 
+  // call scraper loop with this user's id
+  ctx.reply(`Started demo.
+    The bot will check http://localhost:${PORT}/demo.html for price drops every ${DEMO_CRON_INTERVAL_MS / 1000} seconds for ${DEMO_CRON_RUN_LIMIT} times.
+    `);
+
+    
+})
 
 bot.start();
+console.log("Bot server is running. \n");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express web server is running on http://localhost:${PORT}`);
+});
+
+app.get('/api/getDemoData', async (req, res) => {
+  try {
+    db.get('SELECT * FROM demo_product LIMIT 1', (err, row) => {
+      if (err) return res.status(404).json({ error: 'No data found' });
+      res.json(row);
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve data' });
+    console.error(error);
+  }
+});
+
+app.post('/api/postDemoData', async (req, res) => {
+  try {
+    const { pdpTrackingData } = req.body;
+
+    // todo: perform input sanitization later
+    await db.sql(`
+      update demo_product
+        set 
+          pdt_price = '${pdpTrackingData.pdt_price}',
+          misc_isDiscounted = ${pdpTrackingData.misc_isDiscounted},
+          misc_discountedPrice = '${pdpTrackingData.misc_discountedPrice}'
+
+        where pdt_sku = ${pdpTrackingData.pdt_sku}
+        and pdt_simplesku = ${pdpTrackingData.pdt_simplesku}
+      `)
+    res.status(200).send('Demo data updated');
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to add data' });
+    console.error(error);
+  }
+});
 
 function clearObjectValues(obj) {
-    for (const [key, value] of Object.entries(obj)) {
-        if (typeof value === "string") {
-            obj[key] = "";
-        } else if (typeof value === "number") {
-            obj[key] = 0;
-        } else if (typeof value === "boolean") {
-            obj[key] = false;
-        } else if (Array.isArray(value)) {
-            obj[key] = [];
-        } else if (typeof value === "object" && value !== null) {
-            obj[key] = {};
-        } else {
-            obj[key] = null; // Set to null for unhandled types
-        }
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof value === "string") {
+      obj[key] = "";
+    } else if (typeof value === "number") {
+      obj[key] = 0;
+    } else if (typeof value === "boolean") {
+      obj[key] = false;
+    } else if (Array.isArray(value)) {
+      obj[key] = [];
+    } else if (typeof value === "object" && value !== null) {
+      obj[key] = {};
+    } else {
+      obj[key] = null; // Set to null for unhandled types
     }
-    return obj;
+  }
+  return obj;
 }
 
 
 const CRON_INTERVAL_MS = 4 * 60 * 60 * 1000;
 
-function firstScrape() {}
+function firstScrape() { }
 function scrapeLoop() {
-// setTimeout(scrapeDaraz(), CRON_INTERVAL_MS);
+  // setTimeout(scrapeDaraz(), CRON_INTERVAL_MS);
 }
 
-function getNumericPrice(str) {
+export function getNumericPrice(str) {
   return Number(str.replace(/Rs\.?\s?|,|\s/g, ""));
 }
 
-console.log(getNumericPrice("Rs. 9,000"));
-
 async function scrapeDaraz(url) {
-    const productData = {
-        id: '',
-        sku: '',
-        name: '',
-        url: '',
-        finalPrice: 0,
-        // properties starting with _ shouldn't be stored in db
-        _undiscountedPrice: 0,
-        _discountPercent: 0
-    };
+  const productData = {
+    id: '',
+    sku: '',
+    name: '',
+    url: '',
+    finalPrice: 0,
+    // properties starting with _ shouldn't be stored in db
+    _undiscountedPrice: 0,
+    _discountPercent: 0
+  };
 
   const browser = await puppeteer.launch({
     executablePath: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
@@ -74,12 +131,12 @@ async function scrapeDaraz(url) {
   const page = await browser.newPage();
 
   // const url="https://www.daraz.com.np/products/m160-led-mouse-with-rgb-led-light-i128305627-s1035567228.html";
-  await page.goto(url, {timeout: 2*60*1000});
-  
-  await page.setViewport({width: 1080, height: 1024});
+  await page.goto(url, { timeout: 2 * 60 * 1000 });
+
+  await page.setViewport({ width: 1080, height: 1024 });
 
   // TODO: make dummy site have form for changing product data. 
-//   this will then be saved to pdpTrackingData globally-scoped obj 
+  //   this will then be saved to pdpTrackingData globally-scoped obj 
 
   // INFO: each product page in daraz has a globally-scoped obj pdpTrackingData 
   // that has data about the product
@@ -87,7 +144,7 @@ async function scrapeDaraz(url) {
     return typeof pdpTrackingData !== 'undefined' ? pdpTrackingData : null;
   });
 
-  
+
   productData.url = url;
   productData.id = loggedData.pdt_sku;
   productData.sku = loggedData.pdt_simplesku;
@@ -96,14 +153,14 @@ async function scrapeDaraz(url) {
   productData.finalPrice = getNumericPrice(await page.$eval('.pdp-price_type_normal', el => el.textContent));
 
   // maybe notify user about discount %
-  if(productData.finalPrice < productData._undiscountedPrice) {
+  if (productData.finalPrice < productData._undiscountedPrice) {
     console.log("discount");
-    
-      // todo: prob add condiion to check if element w/ this discount class exists or not
-      productData.discountPercent = Number(await page.$eval('.pdp-product-price__discount', el => el.textContent.replace(/-|%/g, '')));
+
+    // todo: prob add condiion to check if element w/ this discount class exists or not
+    productData.discountPercent = Number(await page.$eval('.pdp-product-price__discount', el => el.textContent.replace(/-|%/g, '')));
   }
-  
-  
+
+
   // don't close if scraping multiple pages? maybe create separate fn to close browser & call when last product reached
   await browser.close();
   return productData;
--- commands.sql
+++ commands.sql
@@ -4,3 +4,15 @@ CREATE TABLE products (
     url TEXT NOT NULL,            
     prevPrice REAL    
 );
+
+CREATE TABLE
+  demo_product (
+    pdt_sku INT PRIMARY KEY,
+    pdt_simplesku INT,
+    pdt_name TEXT NOT NULL,
+    pdt_photo TEXT,
+    pdt_price TEXT,
+    -- no native boolean type, but 1 is considered TRUE & 0 is FALSE
+    misc_isDiscounted INT,
+    misc_discountedPrice TEXT
+  );
